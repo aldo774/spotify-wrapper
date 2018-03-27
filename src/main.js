f2230c5312a97e93fdb5fb20a3dc86e77262e5bf
Aldo Soares <soares_aldo@hotmail.com>
2018-03-26 21:56:41 -0300

    [IMP] Inclusão de token para acesso da API do spotify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 global.fetch = require('node-fetch');
 
+const TOKEN_API = 'BQD0oRFpLiEI7w4YQfEbSe3mXDYYY0GIkdVmLLZbQ7E7P' +
+                  'qbTd5OeyZDtSXdHrF_p4J9iW_VD7BZqimjZbbOpo9xyso' +
+                  'z5Y6R_wviMkgBXcuJ888iN9xZgwbkXK9sCfpMyvAGf1HlGgiMu';
+
+const HEADERS = {
+  headers: {
+    Authorization: `"Bearer ${TOKEN_API}"`,
+  },
+};
+
 export const search = (query, type) => global
-  .fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+  .fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, HEADERS)
   .then(data => data.json());
 
 export const searchArtists = artists => search(artists, 'artist');
