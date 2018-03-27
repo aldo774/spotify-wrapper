90cb8ff8d6dfc94bacf0b758ad9ea89da60d7fcd
Aldo Soares <soares_aldo@hotmail.com>
2018-03-27 00:29:33 -0300

    [IMP] Transpilação da lib para ES5 utilizando babel e criação de versão 'UMD' com webpack

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 export const API_URL = 'https://api.spotify.com/v1';
 
-const TOKEN_API = 'BQD0oRFpLiEI7w4YQfEbSe3mXDYYY0GIkdVmLLZbQ7E7P' +
-                  'qbTd5OeyZDtSXdHrF_p4J9iW_VD7BZqimjZbbOpo9xyso' +
-                  'z5Y6R_wviMkgBXcuJ888iN9xZgwbkXK9sCfpMyvAGf1HlGgiMu';
+const TOKEN_API = 'BQDRUKfbRXlGBqFO5XHI_a99PMkCi5yDSeVJ' +
+                  'N-S6VSHoflnOGUs9054q5Pb8yZu1nslgFayt' +
+                  'IHyE8TWP_JP48b5_MFHBCJ9aVwStSUeCK641' +
+                  'n5-e6AAqBaChUKThNUJ-z7bUl545sr5y';
 
 export const HEADERS = {
   headers: {
---
+++ webpack.config.babel.js
@@ -0,0 +1,21 @@
+import { join } from 'path';
+
+const include = join(__dirname, 'src');
+
+export default {
+  entry: "./src/index",
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include},
+    ]
+  }
+}
